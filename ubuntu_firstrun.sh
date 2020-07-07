6903d78d9abf6078806d8157d4c2905664794548
Tomasz Giba <tomaszgiba@users.noreply.github.com>
2020-07-07 18:17:37 +0200

    Update ubuntu_firstrun.sh

--- ubuntu_firstrun.sh
+++ ubuntu_firstrun.sh
@@ -2,12 +2,10 @@
 
 { # this ensures the entire script is downloaded #
 
-sudo apt install -y curl ssh tree zsh vim git htop mc xclip docker docker-compose virtualbox
-
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
-
-snap install code
-snap install slack
+sudo apt install -y curl ssh tree zsh vim git htop mc xclip docker docker-compose virtualbox \ 
+&& sudo usermod -aG docker $USER \ 
+&& sudo systemctl enable docker \ 
+&& snap install code \ 
+&& snap install slack
 
 } # this ensures the entire script is downloaded #
