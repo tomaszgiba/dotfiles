c9f5914ea348a55eedd2cff3822908c7e338caa9
Tomasz Giba <tomaszgiba@users.noreply.github.com>
2020-07-08 17:05:35 +0200

    Update ubuntu_dev.sh

--- ubuntu_dev.sh
+++ ubuntu_dev.sh
@@ -10,8 +10,13 @@ while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 
    sleep 5
 done
 
+# Install Vundle for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Enable Docker
 sudo usermod -aG docker $USER  && sudo systemctl enable docker 
 
+# Install Slack + VSCode
 sudo snap install code --classic && sudo snap install slack
 
 } # this ensures the entire script is downloaded #
