989d5035cae63bd351516035c68e9040cd68db0b
Tomasz Giba <tomaszgiba@users.noreply.github.com>
2020-07-08 12:42:06 +0200

    Update ubuntu_firstrun.sh

--- ubuntu_firstrun.sh
+++ ubuntu_firstrun.sh
@@ -2,15 +2,6 @@
 
 { # this ensures the entire script is downloaded #
 
-sudo apt install -y curl ssh tree zsh vim git htop mc xclip docker docker-compose virtualbox
-
-while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-   echo Waiting for other instances of apt to complete...
-   sleep 5
-done
-
-sudo usermod -aG docker $USER  && sudo systemctl enable docker 
-
-sudo snap install code --classic && sudo snap install slack
+sudo apt install -y curl ssh tree zsh vim git htop mc xclip gnome-tweaks
 
 } # this ensures the entire script is downloaded #
