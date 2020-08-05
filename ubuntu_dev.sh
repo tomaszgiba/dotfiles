5f292c01461f5a3886ea4c57e463e1bca4b961c5
Tomasz Giba <tomaszgiba@users.noreply.github.com>
2020-08-05 17:00:14 +0200

    Update ubuntu_dev.sh

--- ubuntu_dev.sh
+++ ubuntu_dev.sh
@@ -4,12 +4,6 @@
 
 sudo apt install -y docker docker-compose virtualbox 
 
-# wait for apt to finish
-while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-   echo Waiting for other instances of apt to complete...
-   sleep 5
-done
-
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
