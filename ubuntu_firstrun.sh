8badb9f7b75f4622f5fa5873198fc95e4202a564
Tomasz Giba <tomaszgiba@users.noreply.github.com>
2020-08-05 16:15:35 +0200

    Update ubuntu_firstrun.sh

--- ubuntu_firstrun.sh
+++ ubuntu_firstrun.sh
@@ -2,6 +2,18 @@
 
 { # this ensures the entire script is downloaded #
 
-sudo apt install -y ssh jq tree zsh vim git htop mc xclip gnome-tweaks
+# install cli basics
+sudo apt install -y ssh jq tree zsh vim git htop mc
+
+# ..wait for apt to finish 
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+   echo Waiting for other instances of apt to complete...
+   sleep 5
+done
+
+# check if Gnome is present
+if ! [ "$(command -v gnome-shell)" ]; then
+  sudo apt install -y xclip gnome-tweaks
+fi
 
 } # this ensures the entire script is downloaded #
