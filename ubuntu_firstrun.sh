b96a1207fdcc418ff86cf531adc1009c0ae7ed7f
Tomasz Giba <tomasz.giba@gmail.com>
2020-10-18 13:05:08 +0200

    Update ubuntu_firstrun.sh

--- ubuntu_firstrun.sh
+++ ubuntu_firstrun.sh
@@ -3,6 +3,6 @@
 { # this ensures the entire script is downloaded #
 
 # install cli basics
-sudo apt install -y ssh jq tree zsh vim git htop mc libpq-dev xmlsec1 libxml2 libxmlsec1 libxslt1-dev libxmlsec1-dev xclip gnome-tweaks
+sudo apt install -y ssh jq tree zsh vim git htop mc libpq-dev xmlsec1 libxml2 libxmlsec1 libxslt1-dev libxmlsec1-dev
 
 } # this ensures the entire script is downloaded #
